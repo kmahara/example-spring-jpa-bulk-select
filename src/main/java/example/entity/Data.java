c52364412ca610a1aa92875586149299cbb6c379
Kenkichi Mahara <mahara@trasis.jp>
2021-11-16 20:18:59 +0900

    update

--- src/main/java/example/entity/Data.java
+++ src/main/java/example/entity/Data.java
@@ -4,12 +4,14 @@ import java.io.Serializable;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
 @Entity
 @Getter
 @Setter
+@EqualsAndHashCode
 public class Data implements Serializable {
   private static final long serialVersionUID = 1L;
 
